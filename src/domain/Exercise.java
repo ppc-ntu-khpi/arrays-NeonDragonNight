8d91be1718c312e899dd148ef80904b3fd629a33
Без'язичний ростислав <159633985+NeonDragonNight@users.noreply.github.com>
2025-04-14 11:52:39 +0300

    done

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,27 @@
 package domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static int Calculate(int[] array){
+
+        // Підрахунок кількості кожного числа
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : array) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Пошук числа з максимальною кількістю повторень
+        int mostFrequent = array[0];
+        int maxCount = 0;
+        for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                mostFrequent = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        return (int) (mostFrequent);
     }
 }
